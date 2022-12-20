2818894098ac53f3bdda7984041dc9a503fce789
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-20 01:33:29 -0300

    feat: configure app to get content from words.txt

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import sys
 
 class App:
     INI_FILE = "app.ini"
+    WORDS_FILE = "words.txt"
 
     def __load_ini_file(self, filename):
         full_path = os.path.abspath(os.path.join("./src", filename))
@@ -22,9 +23,25 @@ class App:
                 path_config_file))
             return False
 
+    def __load_words_file(self, filename):
+        try:
+            full_path = os.path.abspath(os.path.join("./src/utils/", filename))
+            path_words_file = full_path if os.path.isfile(full_path) else \
+            os.path.abspath(os.path.join(
+                os.path.dirname(sys.executable), filename))
+            self.words = open(path_words_file, 'r', encoding='utf-8')
+            return True
+        except Exception as e:
+            logging.error(f"Failed to open words file: {e}", exc_info=True)
+            return False
+
     def __init__(self):
         if self.__load_ini_file(self.INI_FILE):
-            self.__run()
+            if self.__load_words_file(self.WORDS_FILE):
+                self.__run()
 
     def __run(self):
-        print(self.config["app"]["app_version"])
+        print(f'''{self.config["app"]["app_name"]} - {self.config["app"]["app_version"]}
+Developed by: {self.config["app"]["app_authors"]}''')
+        print(self.words.read())
+        self.words.close()

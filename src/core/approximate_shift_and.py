fb85ec5481fbe7b62ee45fe787504e89da9890d8
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-12-21 23:15:09 -0300

    feat: primary version of approximate shift-and

--- src/core/approximate_shift_and.py
+++ src/core/approximate_shift_and.py
@@ -5,12 +5,33 @@ import time
 class ApproximateShiftAnd:
     def __init__(self, tokens, pattern) -> None:
         start_time = time.time()
-        matches = 0
+        m = len(pattern)
+        n = len(tokens)
         logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             filename='logs/app.log', filemode='w',)
 
         try:
-            print("# Approximate Shift-And algorithm")
+            print("# Aproximate Shift-And algorithm")
+            matches = 0
+
+            m = len(pattern)
+            n = len(tokens)
+
+            if m > n:
+                print(
+                    "The given tokens are not enough to match pattern.")
+                return
+
+            B = {}
+            for i in range(m):
+                B[pattern[i]] = (B.get(pattern[i], 0) | (1 << i))
+
+            D = 0
+            for i in range(n):
+                D = ((D << 1) | 1) & (B.get(tokens[i], 0))
+                if D & (1 << (m - 1)):
+                    matches += 1
+                    print(f"Found match in position {i - m + 1}")
 
             print(f'Total matches for pattern "{pattern}" : {matches}')
         except Exception as e:
